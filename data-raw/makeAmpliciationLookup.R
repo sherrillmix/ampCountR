2b1724d96dd184992cbd9f12f01870196dff3ea0
sherrillmix <sherrillmix@users.noreply.github.com>
2015-08-31 08:29:35 -0400

    Stored lookup table in R data

---
+++ data-raw/makeAmpliciationLookup.R
@@ -0,0 +1,8 @@
+##File name: makeAmpliciationLookup.R
+##Creation date: Aug 31, 2015
+##Last modified: Mon Aug 31, 2015  08:00AM
+##Created by: scott
+##Summary: Generate the lookup table in /data
+
+amplificationLookup<-countAmplifications(1000,1000)
+save(amplificationLookup,file='data/amplificationLookup.RData')
