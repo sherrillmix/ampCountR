d83889dcb59609dcff4145614ba16ba02b548908
sherrillmix <sherrillmix@users.noreply.github.com>
2015-10-01 00:03:52 -0400

    Updated makefile and skeleton vignette

---
+++ inst/doc/example.R
@@ -0,0 +1,39 @@
+### R code from vignette source 'example.Rnw'
+
+###################################################
+### code chunk number 1: package
+###################################################
+options(keep.source = TRUE, width = 60)
+packageInfo <- packageDescription("ampCountR")
+library(ampCountR)
+packageKeywords<-"visualization, amplification, multiple strand displacement, primer design, estimation"
+
+
+###################################################
+### code chunk number 2: plotDNA (eval = FALSE)
+###################################################
+##   set.seed(1234)
+##   plot(1:10)
+
+
+###################################################
+### code chunk number 3: showPlotDNA
+###################################################
+  set.seed(1234)
+  plot(1:10)
+
+
+###################################################
+### code chunk number 4: plotShortReads (eval = FALSE)
+###################################################
+##   set.seed(1234)
+##   plot(1:20)
+
+
+###################################################
+### code chunk number 5: showShortReads
+###################################################
+  set.seed(1234)
+  plot(1:20)
+
+
--- makefile
+++ makefile
@@ -1,12 +1,18 @@
-all: docs package plots
+VERSION:=$(shell grep Version: DESCRIPTION|sed 's/Version: //')
+NAME:=$(shell grep Package: DESCRIPTION|sed 's/Package: //')
+PACKAGEFILE:=../$(NAME)_$(VERSION).tar.gz
+
+all: $(PACKAGEFILE) README.md
+
+.PHONY: all install
 
 install:
-	R -e 'devtools::install_github("sherrillmix/ampCountR")'
+	R -e 'devtools::install_github("sherrillmix/$(NAME)")'
 
 plots: generatePlots.R
 	Rscript generatePlots.R
 
-docs: R/*.R
+man: R/*.R
 	R -e 'devtools::document()'
 	touch man
 
@@ -15,4 +21,5 @@ inst/doc: vignettes/*.Rnw
 	touch inst/doc
 
 package: docs R/*.R DESCRIPTION
+$(PACKAGEFILE): man R/*.R DESCRIPTION tests/testthat/tests.R inst/doc
 	R -e 'devtools::check();devtools::build()'
